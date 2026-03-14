17195fd35d0b95713cc6f616569c7fe7a4e3b1ba
badalsah <badalsah8541@gmail.com>
2026-03-14 09:48:51 +0530

    database connection and user creation routes completed

--- cmd/students-apis/main.go
+++ cmd/students-apis/main.go
@@ -12,16 +12,28 @@ import (
 
 	"github.com/sahbadal/go-student-api/internal/config"
 	"github.com/sahbadal/go-student-api/internal/http/handlers/student"
+	"github.com/sahbadal/go-student-api/internal/storage/sqlite"
 )
 
 func main() {
 
 	cgf := config.MustLoad()
 
+	// database setup
+
+	storage, err := sqlite.New(cgf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized", slog.String("env", cgf.Env), slog.String("version", "1.0.0"))
+
+	//setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
 
+	//setup server
 	server := http.Server{
 		Addr:    cgf.Address,
 		Handler: router,
